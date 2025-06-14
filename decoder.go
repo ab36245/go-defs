cfffcc527989d44a0774966eb80f9fe8b9f53ae0
Andrew Brown <ab36245@gmail.com>
2025-06-14 15:16:40 +1000

    Added support for maps

--- decoder.go
+++ decoder.go
@@ -6,6 +6,19 @@ type ArrayDecoder interface {
 	GetArray() (ArrayDecoder, error)
 	GetDate() (time.Time, error)
 	GetInt() (int, error)
+	GetMap() (MapDecoder, error)
+	GetObject() (ObjectDecoder, error)
+	GetRef() (Ref, error)
+	GetString() (string, error)
+	Length() int
+}
+
+type MapDecoder interface {
+	GetArray() (ArrayDecoder, error)
+	GetDate() (time.Time, error)
+	GetInt() (int, error)
+	GetKey() (string, error)
+	GetMap() (MapDecoder, error)
 	GetObject() (ObjectDecoder, error)
 	GetRef() (Ref, error)
 	GetString() (string, error)
@@ -16,6 +29,7 @@ type ObjectDecoder interface {
 	GetArray(string) (ArrayDecoder, error)
 	GetDate(string) (time.Time, error)
 	GetInt(string) (int, error)
+	GetMap(string) (MapDecoder, error)
 	GetObject(string) (ObjectDecoder, error)
 	GetRef(string) (Ref, error)
 	GetString(string) (string, error)
--- encoder.go
+++ encoder.go
@@ -6,6 +6,18 @@ type ArrayEncoder interface {
 	PutArray(int, func(ArrayEncoder))
 	PutDate(time.Time)
 	PutInt(int)
+	PutMap(int, func(MapEncoder))
+	PutObject(func(ObjectEncoder))
+	PutRef(Ref)
+	PutString(string)
+}
+
+type MapEncoder interface {
+	PutArray(int, func(ArrayEncoder))
+	PutDate(time.Time)
+	PutInt(int)
+	PutKey(string)
+	PutMap(int, func(MapEncoder))
 	PutObject(func(ObjectEncoder))
 	PutRef(Ref)
 	PutString(string)
@@ -15,6 +27,7 @@ type ObjectEncoder interface {
 	PutArray(string, int, func(ArrayEncoder))
 	PutDate(string, time.Time)
 	PutInt(string, int)
+	PutMap(string, int, func(MapEncoder))
 	PutObject(string, func(ObjectEncoder))
 	PutRef(string, Ref)
 	PutString(string, string)
