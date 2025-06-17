8f3c2f202fe3e5d972f60fe05169eebf462ec280
Andrew Brown <ab36245@gmail.com>
2025-06-17 15:11:53 +1000

    Added bool & float and changed date to time

--- decoder.go
+++ decoder.go
@@ -4,32 +4,38 @@ import "time"
 
 type ArrayDecoder interface {
 	GetArray() (ArrayDecoder, error)
-	GetDate() (time.Time, error)
+	GetBool() (bool, error)
+	GetFloat() (float64, error)
 	GetInt() (int, error)
 	GetMap() (MapDecoder, error)
 	GetObject() (ObjectDecoder, error)
 	GetRef() (Ref, error)
 	GetString() (string, error)
+	GetTime() (time.Time, error)
 	Length() int
 }
 
 type MapDecoder interface {
 	GetArray() (ArrayDecoder, error)
-	GetDate() (time.Time, error)
+	GetBool() (bool, error)
+	GetFloat() (float64, error)
 	GetInt() (int, error)
 	GetMap() (MapDecoder, error)
 	GetObject() (ObjectDecoder, error)
 	GetRef() (Ref, error)
 	GetString() (string, error)
+	GetTime() (time.Time, error)
 	Length() int
 }
 
 type ObjectDecoder interface {
 	GetArray(string) (ArrayDecoder, error)
-	GetDate(string) (time.Time, error)
+	GetBool(string) (bool, error)
+	GetFloat(string) (float64, error)
 	GetInt(string) (int, error)
 	GetMap(string) (MapDecoder, error)
 	GetObject(string) (ObjectDecoder, error)
 	GetRef(string) (Ref, error)
 	GetString(string) (string, error)
+	GetTime(string) (time.Time, error)
 }
--- encoder.go
+++ encoder.go
@@ -4,30 +4,36 @@ import "time"
 
 type ArrayEncoder interface {
 	PutArray(int) (ArrayEncoder, error)
-	PutDate(time.Time) error
+	PutBool(bool) error
+	PutFloat(float64) error
 	PutInt(int) error
 	PutMap(int) (MapEncoder, error)
 	PutObject() (ObjectEncoder, error)
 	PutRef(Ref) error
 	PutString(string) error
+	PutTime(time.Time) error
 }
 
 type MapEncoder interface {
 	PutArray(int) (ArrayEncoder, error)
-	PutDate(time.Time) error
+	PutBool(bool) error
+	PutFloat(float64) error
 	PutInt(int) error
 	PutMap(int) (MapEncoder, error)
 	PutObject() (ObjectEncoder, error)
 	PutRef(Ref) error
 	PutString(string) error
+	PutTime(time.Time) error
 }
 
 type ObjectEncoder interface {
 	PutArray(string, int) (ArrayEncoder, error)
-	PutDate(string, time.Time) error
+	PutBool(string, bool) error
+	PutFloat(string, float64) error
 	PutInt(string, int) error
 	PutMap(string, int) (MapEncoder, error)
 	PutObject(string) (ObjectEncoder, error)
 	PutRef(string, Ref) error
 	PutString(string, string) error
+	PutTime(string, time.Time) error
 }
