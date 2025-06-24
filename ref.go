d6906d56f0aaeaa7ec268e9825b7993353a16a9b
Andrew Brown <ab36245@gmail.com>
2025-06-24 17:11:41 +1000

    Added Date and Time data types

--- ref.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "fmt"
-
-type Ref string
-
-func (r Ref) String() string {
-	return fmt.Sprintf("%T(%s)", r, string(r))
-}
---
+++ types.go
@@ -0,0 +1,24 @@
+package model
+
+import (
+	"fmt"
+	"time"
+)
+
+type Date time.Time
+
+func (d Date) String() string {
+	return fmt.Sprintf("%T(%s)", d, time.Time(d).Format(time.DateOnly))
+}
+
+type Time time.Time
+
+func (t Time) String() string {
+	return fmt.Sprintf("%T(%s)", t, time.Time(t).Format(time.TimeOnly))
+}
+
+type Ref string
+
+func (r Ref) String() string {
+	return fmt.Sprintf("%T(%s)", r, string(r))
+}
