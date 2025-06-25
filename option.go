4aa479a4423c47ce99c42b2d5b1535acaf340653
Andrew Brown <ab36245@gmail.com>
2025-06-25 16:50:18 +1000

    Improved Option type

--- option.go
+++ option.go
@@ -1,6 +1,25 @@
 package model
 
+func EmptyOption[T any]() Option[T] {
+	return Option[T]{}
+}
+
+func NewOption[T any](value T) Option[T] {
+	return Option[T]{
+		isSet: true,
+		value: value,
+	}
+}
+
 type Option[T any] struct {
 	isSet bool
 	value T
+}
+
+func (o Option[T]) IsSet() bool {
+	return o.isSet
+}
+
+func (o Option[T]) Value() T {
+	return o.value
 }
